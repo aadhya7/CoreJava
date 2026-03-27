fdd5e3901bdea1d551ef6bff29533f48f2a0b97a
Sowmya R <sowmyamanju68@gmail.com>
2026-03-27 07:32:18 +0530

    commit 04

---
+++ src/Sowmya/ForLoopPrograms/MultiplesOf_7_Table_Untill_100.java
@@ -0,0 +1,24 @@
+package Sowmya.ForLoopPrograms;
+
+//for:write a program to print result of multiple of 7 till 100th number
+/*
+Multiples of 7 till 100 are
+7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98.
+ */
+
+/*
+Solution
+Intilization i=7
+Condition i<=100
+Updation- difference between 2 number is 7 so i=i+7
+ */
+public class MultiplesOf_7_Table_Untill_100 {
+
+    public static void main(String[] args) {
+for(int i=7;i<=100;i=i+7)
+{
+    System.out.println(i);
+}
+
+    }
+}
