771d974448090a8e13928edcb6b23d623ad8be55
Sowmya R <sowmyamanju68@gmail.com>
2026-04-04 15:00:11 +0530

    commit 17

---
+++ src/Sowmya/EvenOrOddProgram.java
@@ -0,0 +1,26 @@
+package Sowmya;
+
+//Java program to Find Odd or Even number
+
+//Solution
+
+
+public class EvenOrOddProgram {
+    public static void main(String[] args) {
+
+        int num=2;
+        if(num%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("ODD");
+        }
+
+
+    }
+
+
+
+}
