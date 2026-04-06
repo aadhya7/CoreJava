d7119de04c16d37078de824e5f9e261f9b57baa2
Sowmya R <sowmyamanju68@gmail.com>
2026-04-06 11:29:07 +0530

    commit 22

--- src/Sowmya/SwapTwoNumbers.java
+++ src/Sowmya/SwapTwoNumbers.java
@@ -1,5 +1,19 @@
 package Sowmya;
 //Java program to swap two numbers without using third variable
-
+/*
+Store the sum of a and b in a (a = a + b).
+Get the original value of a, that is (sum - original value of b)and store it in b (b = a - b).
+Get the original value of b, that is (sum - original value of a)and store it in a (a = a - b).
+ */
 public class SwapTwoNumbers {
+    public static void main(String[] args) {
+
+        int a=1;
+        int b=2;
+        System.out.println("a="+a + "b=" +b) ;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a  + "b=" +b);
+    }
 }
