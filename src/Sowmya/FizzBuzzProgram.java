c0b17e828e377075b122f6429f21e4af2fd09da7
Sowmya R <sowmyamanju68@gmail.com>
2026-04-06 11:16:43 +0530

    commit 21

--- src/Sowmya/FizzBuzzProgram.java
+++ src/Sowmya/FizzBuzzProgram.java
@@ -4,6 +4,9 @@ package Sowmya;
 //when the number is divisible by 5 --> print "Buzz"
 //when the number is divisible by 2 & 5 --> "FizzBuzz"
 //for remaining number print the "number"
+
+//expected: 1 Fizz 3 Fizz Buzz Fizz 7 Fizz 9 FizzBuzz
+//Actual: 1 1 Fizz 2 2 3 3 Fizz 4 4 buzz 5 5
 /*
 Solution
 1.add an if condition when number is divided by 2 and the reminder should be zero
@@ -13,21 +16,35 @@ Solution
 
 public class FizzBuzzProgram {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
-        int num=10;
-
-        if(num%2==0)
-        {
-            System.out.println("Fizz - number is divisible by 2  " + num);
-        }
-         if(num%5==0)
-        {
-            System.out.println("Buzz - number is divisible by 5 "+num);
-        }
-         if(num%2==0 && num%5==0)
-        {
-            System.out.println("FizzBuzz - number is divisible by 2 & 5 " +num);
-        }
+        for (int i = 1; i <= 50; i++) {
+            if (i % 2 == 0 && i% 5==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }  else if (i % 2 == 0) {
+                    System.out.println("Fizz ");
+                } else  {
+                    System.out.println(i);
+                }
+            //System.out.println(i);
+            }
     }
 }
+
+/* output: 1 Fizz 2
+Iteration1:
+i = 1
+if1 -> false
+if2 -> false
+if3 -> false
+--------------------------------------------
+Iteration2:
+i = 2
+if1 -> true
+if2 -> false
+if3 -> false
+
+ */
